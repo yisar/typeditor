94b0803402473ed2b6aaffb238b59ef7c4f41333
132yse <1533540012@qq.com>
2019-04-14 22:50:00 +0800

    日常提交

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 export class Typeditor {
   el: Element
+  content: string
   constructor({ el }) {
     this.el = document.getElementById(el)
+    this.content = ''
+  }
+
+  value(content: string) {
+    return content ? (this.el.innerHTML = content) : this.content
   }
 }
