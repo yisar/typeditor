8f432fc6c98954ae56a69461f27b29948b0a4823
132yse <1533540012@qq.com>
2019-04-19 15:02:53 +0800

    日常提交

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
 export class Typeditor {
-  el: Element
   content: string
-  constructor({ el }) {
-    this.el = document.getElementById(el)
+  el: Element
+  template: string
+  constructor(options: Options) {
+    this.el = document.querySelector(options.el)
     this.content = ''
+    this.init()
+  }
+
+  static template = `
+    <style>
+    @import "https://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    #typeditor{
+      position:relative;
+    }
+    <style>
+  `
+
+  init() {
+    this.el.innerHTML = this.template
   }
 
   value(content: string) {
     return content ? (this.el.innerHTML = content) : this.content
   }
+}
+
+interface Options {
+  el: string
 }
